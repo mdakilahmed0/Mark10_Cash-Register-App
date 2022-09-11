d734266feb42d6247ab5463e5f8e6845c1cd39d2
mdakilahmed0 <mdakilahmed@pm.me>
2022-09-11 16:54:08 +0530

    Fix: Cash amount less than bill amount issue has been fixed.

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
-const billAmount = document.querySelector("#bill-amount")
-const cashAmount = document.querySelector("#cash-amount")
-const checkBtn = document.querySelector("#btnCheck")
-const errorMsg = document.querySelector("#error")
-const amountQuantity = document.querySelectorAll(".no-of-notes")
+const billAmount = document.querySelector("#bill-amount");
+const cashAmount = document.querySelector("#cash-amount");
+const checkBtn = document.querySelector("#btnCheck");
+const errorMsg = document.querySelector("#error");
+const amountQuantity = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
 
-checkBtn.addEventListener("click", validateReturnAmount);
 
-function validateReturnAmount(){
-    if(billAmount.value > 0){
-        if(cashAmount.value >= billAmount.value){
-            const balance = cashAmount.value - billAmount.value;
-            calculateReturnChange(balance);
+
+function validateReturnAmount() {
+    const billAmt = Number(billAmount.value);
+    const cashAmt = Number(cashAmount.value);
+
+    if ((billAmt > 0) && (cashAmt > 0)) {
+        if (cashAmt < billAmt) {
+            showErrorMessage('You seems broke, go and wash plates..')
+        } else if (cashAmt === billAmt) {
+            showErrorMessage('Bill is been paid, No change left, thanks')
+        } else {
+            showErrorMessage('');
+            let balance = cashAmt - billAmt;
+            calculateReturnChange(balance)
         }
-    } 
-        else {
-            showErrorMessage("Check the cash given amount!!")
-        }
+    } else {
+        showErrorMessage('You need to enter both the fields...');
     }
+}
 
 
-function calculateReturnChange(balance){
-    for(let i=0; i<availableNotes.length; i++){
-        const noteQuantity = Math.trunc(balance/availableNotes[i]);
+
+function calculateReturnChange(balance) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const noteQuantity = Math.trunc(balance / availableNotes[i]);
         balance = balance % availableNotes[i];
         amountQuantity[i].innerText = noteQuantity;
     }
 }
 
-function showErrorMessage(msg){
-    errorMsg.innerText = msg
+function showErrorMessage(msg) {
+    errorMsg.innerText = msg;
 }
+
+checkBtn.addEventListener("click", validateReturnAmount);
